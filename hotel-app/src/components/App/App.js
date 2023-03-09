86fe687b310d2968d7a6e45737f61e6c55f6eb6f
sanglc98 <Sanglc98x@gmail.com>
2023-03-09 16:09:08 +0000

    9/3

---
+++ hotel-app/src/components/App/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import './App.css';
+import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
+import Preferences from '../Preferences/Preferences';
+
+function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route  path="/dashboard" element={<Dashboard />} />
+          <Route  path="/preferences" element={<Preferences />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
