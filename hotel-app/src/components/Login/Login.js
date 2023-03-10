1f6204b22ca8d767d891c3d8301d0a5e1450bcf3
sanglc98 <Sanglc98x@gmail.com>
2023-03-10 14:40:51 +0000

    api

--- hotel-app/src/components/Login/Login.js
+++ hotel-app/src/components/Login/Login.js
@@ -1,17 +1,42 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 import './Login.css';
 
-export default function Login() {
+async function loginUser(credentials) {
+ return fetch('https://3080-sanglc98-hotelreact-nlbvcaa0194.ws-us90.gitpod.io/login', {
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+   body: JSON.stringify(credentials)
+ })
+   .then(data => data.json())
+}
+
+export default function Login({ setToken }) {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password
+    });
+    setToken(token);
+  }
+
   return(
     <div className="login-wrapper">
       <h1>Please Log In</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" />
+          <input type="text" onChange={e => setUserName(e.target.value)} />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input type="password" onChange={e => setPassword(e.target.value)} />
         </label>
         <div>
           <button type="submit">Submit</button>
@@ -20,3 +45,7 @@ export default function Login() {
     </div>
   )
 }
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
+};
